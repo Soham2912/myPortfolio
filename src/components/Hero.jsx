5978e9f18854903c82973d3bf9d45b0538b7406a
Soham2912 <sohamlahoti@gmail.com>
2024-08-28 08:03:10 +0530

    Add Hero component and update Navbar component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='border-b border-neutral-900 pb-4 lg:mb-36' >
+        <div className='flex flex-wrap'>
+            <div className='w-full lg:w-1/2'>
+            <div className='flex flex-col items-center lg:items-start'>
+              <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl '>Soham Lahoti</h1>
+            <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>
+              Web Developer
+            </span>
+            <p ></p>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
